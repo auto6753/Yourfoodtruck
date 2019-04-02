dfa5abbdf3d30e6ebeef8c9734e4bbe76e0e75ab
auto6753 <auto67532@naver.com>
2019-04-02 19:33:48 +0900

    커스터머 컨트롤러에 memservice -> loginservice 수정 회원탈퇴부분 수정

--- foodtruck/src/main/java/com/food/project/controller/CustomerController.java
+++ foodtruck/src/main/java/com/food/project/controller/CustomerController.java
@@ -11,14 +11,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.food.project.service.MemberService;
+import com.food.project.domain.CustomerVO;
+import com.food.project.service.LoginService;
 
 import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @Controller
 @RequestMapping(value = "/customer", method = RequestMethod.GET)
 public class CustomerController {
-	private MemberService service;
+	private LoginService service;
 	
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public String mypage(Locale locale, Model model) {
@@ -57,9 +58,11 @@ public class CustomerController {
 	
 	//회원탈퇴
 	@RequestMapping(value = "/goodbye", method = RequestMethod.GET)
-	public String goodbye(Locale locale, Model model,@RequestParam("m_mail") String m_mail,HttpServletRequest request) {
+	public String goodbye(Locale locale, Model model,HttpServletRequest request) {
+		CustomerVO c = new CustomerVO();
+		c=(CustomerVO) request.getSession().getAttribute("sessionid");
 		request.getSession().removeAttribute("sessionid");
-		service.deletemem(m_mail);	
+		service.delete(c.getEmail());	
 		return "customer/goodbye";
 	}
 }
