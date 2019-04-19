90ca4fd796e52c386a20e0cd46923ea8466b07ba
auto6753 <46548421+auto6753@users.noreply.github.com>
2019-04-19 14:15:01 +0900

    Update PaymentController.java

--- foodtruck/src/main/java/com/food/project/controller/PaymentController.java
+++ foodtruck/src/main/java/com/food/project/controller/PaymentController.java
@@ -1,26 +1,51 @@
 package com.food.project.controller;
 
-import org.springframework.http.HttpRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import net.sf.json.*;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.food.project.domain.PaymentVO;
 import com.food.project.service.PaymentService;
 import lombok.AllArgsConstructor;
 
 @Controller
 @AllArgsConstructor
+@RequestMapping(value="/pay")
 public class PaymentController {
 	
 	private PaymentService payService;
-	@RequestMapping(value = "/payment", method = RequestMethod.POST)
-	public String recruit(Model model,HttpRequest request) {
-
-		
-		
-		
-		return "/project";
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = "/insertPayment")
+	@ResponseBody
+	public void payment(@RequestBody String param) {
+		List<Map<String,Object>> paymentMap = new ArrayList<Map<String,Object>>();
+		paymentMap = JSONArray.fromObject(param);
+		int a=payService.insertPaymentList(paymentMap);
+		if(a==0) System.out.println("Error");
+		else System.out.println("Success");
 	}
-	
+	@RequestMapping(value="/insertOrder",method=RequestMethod.POST)
+	public void insertOrder(Model model, HttpServletRequest request) {
+	}
+	@RequestMapping(value="/afterOrder",method=RequestMethod.POST)
+	public String afterOrder() {
+		return "seller/order/afterOrder";
+	}
+	@RequestMapping(value="/payck",method=RequestMethod.POST)
+	@ResponseBody
+	public String payck(Model model,PaymentVO vo) {
+		System.out.println(vo.getTruck_code());
+		System.out.println(vo.getPayment_telephone());
+		return "success";
+	}
 }
