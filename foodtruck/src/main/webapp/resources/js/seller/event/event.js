9651d00e46e548b29253742614a66ac2bef41a4b
auto6753 <46548421+auto6753@users.noreply.github.com>
2019-05-07 10:59:32 +0900

    Update event.js

--- foodtruck/src/main/webapp/resources/js/seller/event/event.js
+++ foodtruck/src/main/webapp/resources/js/seller/event/event.js
@@ -1,9 +1,26 @@
 /**
  * 
  */
-	$(function () {	
-		tab('#tab',0);	
-	});
+//	var index = 1;
+	function addEventBtn() {
+		document.getElementById("submitAddEvent").click();
+
+	}
+	
+	function editEventBtn() {
+		document.getElementById("submitEditEvent").click();
+
+	}
+	
+	function changeAttr(selectTag) {
+		var selectVal = $(selectTag).val();
+		
+		if(selectVal != "" && selectVal != null && selectVal != undefined){
+			$(selectTag).next().next().attr("disabled", false);
+		} else {
+			$(selectTag).next().next().attr("disabled", true);
+		}
+	}
 
 	function tab(e, num){
 	    var num = num || 0;
@@ -68,7 +85,7 @@
 	
 	// 딤처리 팝업 레이어(시작)
 	function layer_popup(el) {
-
+		
 		var $el = $(el); //레이어의 id를 $el 변수에 저장
 		var isDim = $el.prev().hasClass('dimBg'); //dimmed 레이어를 감지하기 위한 boolean 변수
 
@@ -89,6 +106,7 @@
 				left : 0
 			});
 		}
+		
 
 		$el.find('button.btn-layerClose').click(function() {
 			isDim ? $('.dim-layer').fadeOut() : $el.fadeOut(); // 닫기 버튼을 클릭하면 레이어가 닫힌다.
@@ -144,6 +162,17 @@
 		var div = document.createElement('div');
 		div.innerHTML = document.getElementById('pre_set').innerHTML;
 		document.getElementById('field').appendChild(div);
+//		$(div).children("select").attr("id", "selectMenu"+index);
+//		index++;
+	}
+	
+	function add_item2(){
+		// pre_set 에 있는 내용을 읽어와서 처리..
+		var div = document.createElement('div');
+		div.innerHTML = document.getElementById('pre_set2').innerHTML;
+		document.getElementById('field2').appendChild(div);
+//		$(div).children("select").attr("id", "selectMenu"+index);
+//		index++;
 	}
 
 	function remove_item(obj){
@@ -151,17 +180,164 @@
 		document.getElementById('field').removeChild(obj.parentNode);
 	}
 	
+	function remove_item2(obj){
+		// obj.parentNode 를 이용하여 삭제
+		document.getElementById('field2').removeChild(obj.parentNode);
+	}
+	
+	function alerttest(){
+		alert("a");
+	}
+	
 	$(document).ready(function(){
+		$("#previewId").mouseenter(function(){
+			$("#imgControlBox").fadeIn();
+			$("#noImage,#defaultImg,#prev_previewId").css("position", "relative");
+			$("#noImage,#defaultImg,#prev_previewId").css("top", "-50px");
+		});
+		
+		$("#previewId").mouseleave(function(){
+			$("#imgControlBox").fadeOut(200);
+			setTimeout(function() {
+				$("#noImage,#defaultImg,#prev_previewId").css("top", "0");
+			}, 200);
+			
+/*			var opacityVal;
+			var checkOpacity =
+				setInterval(function() {
+					opacityVal = $("#imgControlBox").css("opacity");
+					if(opacityVal == 0) {
+						$("#noImage").css("top", "0");		
+					clearInterval(checkOpacity);
+					}
+				}, 0);
+*/
+		});
+		
+		$("#previewId2").mouseenter(function(){
+			$("#imgControlBox2").fadeIn();
+			$("#noImage2,#defaultImg2,#prev_previewId2").css("position", "relative");
+			$("#noImage2,#defaultImg2,#prev_previewId2").css("top", "-50px");
+		});
+		
+		$("#previewId2").mouseleave(function(){
+			$("#imgControlBox2").fadeOut(200);
+			setTimeout(function() {
+				$("#noImage2,#defaultImg2,#prev_previewId2").css("top", "0");
+			}, 200);
+		});
+		
+		$("#setDefaultBtn").click(function(){
+			$("#defaultImg").css("display", "");
+			$("#noImage").css("display", "none");
+			$("#uploadImg").val("");
+			$("#prev_previewId").remove();
+			
+			setTimeout(function() {
+				alert("기본이미지로 변경되었습니다.");
+			}, 1);
+		});
+		
+		$("#setDefaultBtn2").click(function(){
+			$("#defaultImg2").css("display", "");
+			$("#noImage2").css("display", "none");
+			$("#uploadImg2").val("");
+			$("#prev_previewId2").remove();
+			
+			setTimeout(function() {
+				alert("기본이미지로 변경되었습니다.");
+			}, 1);
+		});
+		
+		$("#delUploadImg").click(function(){
+			var chkImg = $("#noImage").css("display");
+			
+			$("#defaultImg").css("display", "none");
+			$("#noImage").css("display", "");
+			$("#uploadImg").val("");
+			$("#prev_previewId").remove();
+			
+			if(chkImg == "none"){
+				setTimeout(function() {
+					alert("업로드된 이미지가 삭제되었습니다.");
+				}, 1);
+			} else {
+				alert("삭제할 이미지가 존재하지 않습니다.");
+			}
+		});
+		
+		$("#delUploadImg2").click(function(){
+			var chkImg = $("#noImage2").css("display");
+			
+			$("#defaultImg2").css("display", "none");
+			$("#noImage2").css("display", "");
+			$("#uploadImg2").val("");
+			$("#prev_previewId2").remove();
+			
+			if(chkImg == "none"){
+				setTimeout(function() {
+					alert("업로드된 이미지가 삭제되었습니다.");
+				}, 1);
+			} else {
+				alert("삭제할 이미지가 존재하지 않습니다.");
+			}
+		});
+		
+		$("#cancelAdd, #cancelEdit").click(function(){
+			$("form").each(function(){
+				this.reset();
+				// input type="file"의 경우, IE10을 제외한 모든 브라우저에서 지원
+				// IE10에서는 reset 된 것처럼 보이지만, submit 할 경우 해당 파일이 전송되는 버그 발생 
+				$("#noImage").css("display", "");
+				$("#prev_previewId").remove();
+			});
+		});
+		
+		$("#cancelEdit").click(function(){
+			$("form").each(function(){
+				this.reset();
+				// input type="file"의 경우, IE10을 제외한 모든 브라우저에서 지원
+				// IE10에서는 reset 된 것처럼 보이지만, submit 할 경우 해당 파일이 전송되는 버그 발생 
+				$("#noImage2").css("display", "");
+				$("#prev_previewId2").remove();
+			});
+		});
+		
+		
+		
+		
+		tab('#tab',0);
+		
 		$(".imgUploadBtn").click(function(){
 			document.getElementById("uploadImg").click();
 		});
 		
+		$(".imgUploadBtn2").click(function(){
+			document.getElementById("uploadImg2").click();
+		});
+		
 		$("#addEventBtn").click(function(){
-			alert("이벤트가 등록되었습니다.");
+			var eventName = $("#eventName").val();
+			var beginDate = $("#beginDate").val();
+			var endDate = $("#endDate").val();
+			var target = $("#target").val();
+			
+			
+			$.ajax({
+				type: "post",
+				url: "event",
+				data: {
+					"eventName": eventName
+				},
+				success: function(data){
+					 alert("success");
+				}
+			});
+//			alert("이벤트가 등록되었습니다.");
 		});
 		
 		$("#editEventBtn").click(function(){
-			alert("이벤트가 수정되었습니다.");
+//			alert("이벤트가 수정되었습니다.");
 		});
 		
 		$(".delete").click(function(){
@@ -235,6 +411,86 @@
 				}
 				
 				$("#noImage").css("display", "none");
+				$("#defaultImg").css("display", "none");
+				
+
+				var img = document.createElement("img"); // 크롬은 div에 이미지가 뿌려지지 않기 때문에 자식 Element를 생성
+				img.id = "prev_" + previewId;
+				img.classList.add("obj");
+				img.file = file;
+				img.style.width = '400px'; // 기본 설정된 div의 안에 뿌려지는 효과를 주기 위해서 div 크기와 같은 크기로 지정
+				img.style.height = '300px';
+
+				preview.appendChild(img);
+
+				if (window.FileReader) { // FireFox, Chrome, Opera 확인
+					var reader = new FileReader();
+					reader.onloadend = (function(aImg) {
+						return function(e) {
+							aImg.src = e.target.result;
+						};
+					})(img);
+					reader.readAsDataURL(file);
+				} else { // safari is not supported FileReader
+					// alert('not supported FileReader');
+					if (!document.getElementById("sfr_preview_error_" + previewId)) {
+						var info = document.createElement("p");
+						info.id = "sfr_preview_error_" + previewId;
+						info.innerHTML = "not supported FileReader";
+						preview.insertBefore(info, null);
+					}
+				}
+			}
+		}
+	}
+	
+	function previewImage2(targetObj, previewId) {
+		var preview = document.getElementById(previewId); // div id
+		var ua = window.navigator.userAgent;
+
+		if (ua.indexOf("MSIE") > -1) {// ie일때
+
+			targetObj.select();
+
+			try {
+				var src = document.selection.createRange().text; // get file full
+				// path
+				var ie_preview_error = document.getElementById("ie_preview_error_"
+						+ previewId);
+
+				if (ie_preview_error) {
+					preview.removeChild(ie_preview_error); // error가 있으면 delete
+				}
+
+				var img = document.getElementById(previewId); // 이미지가 뿌려질 곳
+
+				img.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"
+						+ src + "', sizingMethod='scale')"; // 이미지 로딩, sizingMethod는 div에 맞춰서 사이즈를 자동조절 하는 역할
+			} catch (e) {
+				if (!document.getElementById("ie_preview_error_" + previewId)) {
+					var info = document.createElement("<p>");
+					info.id = "ie_preview_error_" + previewId;
+					info.innerHTML = "a";
+					preview.insertBefore(info, null);
+				}
+			}
+		} else { // ie가 아닐때
+			var files = targetObj.files;
+			for (var i = 0; i < files.length; i++) {
+
+				var file = files[i];
+
+				var imageType = /image.*/; // 이미지 파일일 경우에만 뿌려줌
+				if (!file.type.match(imageType))
+					continue;
+				
+				var prevImg = document.getElementById("prev_" + previewId); // 이전에 미리보기가 있다면 삭제
+				if (prevImg) {
+					preview.removeChild(prevImg);
+				}
+				
+				$("#noImage2").css("display", "none");
+				$("#defaultImg2").css("display", "none");
 
 				var img = document.createElement("img"); // 크롬은 div에 이미지가 뿌려지지 않기 때문에 자식 Element를 생성
 				img.id = "prev_" + previewId;
