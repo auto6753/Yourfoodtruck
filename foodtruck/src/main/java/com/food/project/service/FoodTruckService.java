228967a03fee98f8c3aba3c8200b9f36524edfa0
Tak-kuen <32920482+Tak-kuen@users.noreply.github.com>
2019-05-07 12:10:25 +0900

    Create FoodTruckService.java

---
+++ foodtruck/src/main/java/com/food/project/service/FoodTruckService.java
@@ -0,0 +1,13 @@
+package com.food.project.service;
+
+import java.util.ArrayList;
+import com.food.project.domain.FoodTruckVO;
+import com.food.project.domain.ReviewVO;
+
+public interface FoodTruckService {
+  public ArrayList<FoodTruckVO> getFoodTruckList();
+    public FoodTruckVO getFoodTruck(String truck_code);
+    public FoodTruckVO insertFoodTruck(FoodTruckVO vo);
+    public void insertReview(ReviewVO vo);
+    public ArrayList<ReviewVO> getReviewList(String truck_code);
+}
