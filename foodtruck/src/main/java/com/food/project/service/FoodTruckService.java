d044c17b79b9cbda692ac51122ab43f81c0682a7
Tak-kuen <32920482+Tak-kuen@users.noreply.github.com>
2019-05-07 12:11:11 +0900

    Update FoodTruckService.java

--- foodtruck/src/main/java/com/food/project/service/FoodTruckService.java
+++ foodtruck/src/main/java/com/food/project/service/FoodTruckService.java
@@ -5,9 +5,9 @@ import com.food.project.domain.FoodTruckVO;
 import com.food.project.domain.ReviewVO;
 
 public interface FoodTruckService {
-  public ArrayList<FoodTruckVO> getFoodTruckList();
-    public FoodTruckVO getFoodTruck(String truck_code);
-    public FoodTruckVO insertFoodTruck(FoodTruckVO vo);
-    public void insertReview(ReviewVO vo);
-    public ArrayList<ReviewVO> getReviewList(String truck_code);
+	public ArrayList<FoodTruckVO> getFoodTruckList();
+	public FoodTruckVO getFoodTruck(String truck_code);
+	public FoodTruckVO insertFoodTruck(FoodTruckVO vo);
+	public void insertReview(ReviewVO vo);
+	public ArrayList<ReviewVO> getReviewList(String truck_code);
 }
