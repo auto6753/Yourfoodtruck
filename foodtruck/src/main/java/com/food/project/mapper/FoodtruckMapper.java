b772ffda15ea716e2d32310079939bb03a446f5b
tmxk7656 <tmxk5765@naver.com>
2019-05-07 09:48:27 +0900

    트럭검색 상세보기 리뷰 쓰기 삭제 평점표시 완료

--- foodtruck/src/main/java/com/food/project/mapper/FoodtruckMapper.java
+++ foodtruck/src/main/java/com/food/project/mapper/FoodtruckMapper.java
@@ -1,12 +1,21 @@
 package com.food.project.mapper;
 
 import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Delete;
+
 import com.food.project.domain.FoodTruckVO;
+import com.food.project.domain.ReviewVO;
 
 public interface FoodtruckMapper {
 	public ArrayList<FoodTruckVO> getFoodTruckList();
 	public FoodTruckVO getFoodTruck(String truck_code);
 	public void insertFoodTruck(FoodTruckVO vo);
 	public FoodTruckVO getFoodTruckE(String email);
+	public void insertReview(ReviewVO vo);
+	public ArrayList<ReviewVO> getReviewList(String truck_code);
+	
+	@Delete("delete from review where review_code =#{review_code}")
+	public void reviewdelete(ReviewVO vo);
 	
 }
